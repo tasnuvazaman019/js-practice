43918fa1015442b076cf4579e4933c5480b3653c
Tasnuva Zaman <68296247+tasnuvazaman019@users.noreply.github.com>
2023-09-04 15:55:54 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function textChange(){
+	document.getElementById('h2').innerHTML="Assalamualaikum World";
+}
+
+function textChangeA(){
+	document.getElementById('h2a').innerHTML="Hello World";
+}
+
+function textChangeB(){
+	document.getElementById('h2b').innerHTML="How are you?";
+}
+
+function textChangeC(){
+	document.getElementById('h2c').innerHTML="Where do you live?";
+}
+
+function textChangeD(){
+	document.getElementById('h2d').innerHTML="What do you do for living?";
+}
+
+function textChangeE(){
+	document.getElementById('h2e').innerHTML="What is your first name?";
+}
+
+function textChangeF(){
+	document.getElementById('h2f').innerHTML="What is you last name?";
+}
+
+function textChangeG(){
+	document.getElementById('h2g').innerHTML="Do you have any nick name?";
+}
+
+function textChangeI(){
+	document.getElementById('h2i').innerHTML="you have any hobby?";
+}
+
+function textChangeH(){
+	document.getElementById('h2h').innerHTML="Where do you see yourself after 5 years?";
+}
